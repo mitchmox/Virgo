1b638026951ac31eab5b0d12273cb54bc504706a
mitchmox <mitchmox@me.com>
2016-03-31 22:35:56 -0500

    Added actual Swift code.
    Followed directions at to attempt to make something. I don’t have a
    Javascript file and I am tired so i gave up.
    https://developer.apple.com/library/tvos/documentation/General/Conceptua
    l/AppleTV_PG/YourFirstAppleTVApp.html

--- Virgo.playground/Contents.swift
+++ Virgo.playground/Contents.swift
@@ -1,5 +1,13 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import TVMLKit
 
 var str = "Hello, playground"
+2+2
+
+var firstName = "Michael"
+var lastName = "Tremel"
+
+firstName+" "+lastName
+
